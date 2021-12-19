8f947c1e8de8b56391a9a15b16a94b79de6b09ad
MetinAgayev <matinga@code.edu.az>
2021-12-19 16:10:03 +0400

    Add project files.

---
+++ ConsoleApp28/Car.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp28
+{
+    class Car : Vehicle
+    {
+        public void Drive()
+        {
+            Console.WriteLine("Car is driving");
+        }
+    }
+}
---
+++ ConsoleApp28/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsoleApp28
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car();
+            car.Drive();
+        }
+    }
+}
